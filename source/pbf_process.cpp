f46bfbd8554b44086058526cb705eb6aad35cd13
MSABuschman <buschman@umich.edu>
2024-04-07 10:42:43 -0700

    Enable OpenMP

--- source/pbf_process.cpp
+++ source/pbf_process.cpp
@@ -36,31 +36,23 @@ std::vector<double> PbfProcess::GetSpectrum(std::vector<double>& E_bins) {
     std::cout << "Compute spectrum with GSL ..." << std::endl;
     double rmin=0, rmax=0;
     GetBoundaries(&rmin, &rmax);
-    //nscool->DetermineDeltaTInfty(this);
-
-#if SCHEME<CQUAD
-    gsl_integration_workspace* w = gsl_integration_workspace_alloc(1000);
-#elif SCHEME==CQUAD
-    gsl_integration_cquad_workspace* w = gsl_integration_cquad_workspace_alloc(10000);
-#elif SCHEME==ROMBERG
-    gsl_integration_romberg_workspace* w = gsl_integration_romberg_alloc(19);
-#else
-    std::cout << "No apropriate integrator selected during compilation!"
-              << std::endl;
-    return NULL;
-#endif
-    gsl_function F;
-    F.function = &PbfProcess::GslIntegrand;
-
     std::vector<double> spectrum(E_bins.size(), 0.);
-    for (unsigned int i = 0; i < E_bins.size(); ++i) {
+#if SCHEME==QAPG
+    nscool->DetermineDeltaTInfty(this);
+#endif
+
+#pragma omp parallel for
+    for (size_t i = 0; i < E_bins.size(); ++i) {
         GslIntegrationParams gsl_params;
         gsl_params.pthis = this;
         gsl_params.E = E_bins[i];
+        gsl_function F;
+        F.function = &PbfProcess::GslIntegrand;
         F.params = &gsl_params;
 
 #if SCHEME==QAGS
         double error;
+        gsl_integration_workspace* w = gsl_integration_workspace_alloc(1000);
         gsl_integration_qags(&F, rmin, rmax, 0.1, 1e-2, 1000, w, &spectrum[i],
                              &error);
 #elif SCHEME==QAPG
@@ -74,15 +66,20 @@ std::vector<double> PbfProcess::GetSpectrum(std::vector<double>& E_bins) {
         }
         pts.push_back(rmax);
         double error;
+        gsl_integration_workspace* w = gsl_integration_workspace_alloc(1000);
         gsl_integration_qagp(&F, &pts[0], pts.size(), 1e-2, 1e-3, 1000, w,
                              &spectrum[i], &error);
 #elif SCHEME==CQUAD
         double error;
         size_t neval;
-        gsl_integration_cquad(&F, rmin, rmax, 0, 1e-6, w, &spectrum[i],
-                              &error,  &neval);
+        gsl_integration_cquad_workspace* w =
+                gsl_integration_cquad_workspace_alloc(10000);
+        gsl_integration_cquad(&F, rmin, rmax, 0, 1e-6, w, &spectrum[i], &error,
+                              &neval);
 #elif SCHEME==ROMBERG
         size_t neval;
+        gsl_integration_romberg_workspace* w =
+                gsl_integration_romberg_alloc(19);
         gsl_integration_romberg(&F, rmin, rmax, 0, 1e-3, &spectrum[i], &neval,
                                 w);
 #endif
@@ -97,10 +94,10 @@ std::vector<double> PbfProcess::GetSpectrum(std::vector<double>& E_bins,
     double rmin=0, rmax=0;
     GetBoundaries(&rmin, &rmax);
     std::vector<double> r = CreateVector(rmin, rmax, N);
-    //nscool->DetermineDeltaTInfty(this);
 
     std::vector<double> spectrum(E_bins.size(), 0.);
-    for (unsigned int i = 0; i < E_bins.size(); ++i) {
+#pragma omp parallel for
+    for (size_t i = 0; i < E_bins.size(); ++i) {
         for (int j = 0; j < N; ++j) {
             spectrum[i] += Integrand(r[j], E_bins[i], this);
         }
@@ -122,7 +119,7 @@ void PbfProcess::GetBoundaries(double* rmin, double* rmax) {
 void PbfProcess::Normalize(std::vector<double>& x, std::vector<double>& y) {
     double sum = std::accumulate(y.begin(), y.end(), 0.);
     double norm = 1./(sum*(x.back()-x[0]));
-    for (unsigned int i = 0; i < y.size(); ++i) {
+    for (size_t i = 0; i < y.size(); ++i) {
         y[i] *= norm;
     }
 }
